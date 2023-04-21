4714fe97eac5d8bcfaa66f5593b3b6fab0cfa9c5
Midya <midyaso5439@gmail.com>
2023-04-21 21:07:14 +0300

    init

--- brt_service/src/main/java/ru/nexign/brtservice/entity/Tariff.java
+++ brt_service/src/main/java/ru/nexign/brtservice/entity/Tariff.java
@@ -1,51 +1,51 @@
-//package ru.nexign.brtservice.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import lombok.AccessLevel;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
-//import lombok.experimental.FieldDefaults;
-//
-//
-//import java.util.Objects;
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@FieldDefaults(level = AccessLevel.PRIVATE)
-//@Entity
-//@Table(name = "tariffs")
-//@ToString
-//public class Tariff {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    Long id;
-//
-//    String tariffIndex;
-//
-//    String name;
-//
-//    Double priceRubMin;
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        var tariff = (Tariff) o;
-//        return Objects.equals(tariffIndex, tariff.tariffIndex);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(tariffIndex);
-//    }
-//}
+package ru.nexign.brtservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "tariffs")
+@ToString
+public class Tariff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String tariffIndex;
+
+    String name;
+
+    Double priceRubMin;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var tariff = (Tariff) o;
+        return Objects.equals(tariffIndex, tariff.tariffIndex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tariffIndex);
+    }
+}
--- cdr_service/src/main/java/ru/nexign/cdrservice/ApplicationCdrService.java
+++ cdr_service/src/main/java/ru/nexign/cdrservice/ApplicationCdrService.java
@@ -10,6 +10,7 @@ public class ApplicationCdrService {
     public static void main(String[] args) {
 
         SpringApplication.run(ApplicationCdrService.class, args);
+
     }
 
 }
